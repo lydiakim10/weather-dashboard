6ce273ffd7448933a51219c65f6e7450f276b4c6
Lydia Kim <lydiakim47@gmail.com>
2022-08-27 22:02:58 -0700

    html and some css added with var in js added

---
+++ script.js
@@ -0,0 +1,9 @@
+var cities = [];
+
+var citySearched = document.querySelector("#citySearches");
+var cityInput = document.querySelector("#cityTyped");
+var currWeather = document.querySelector("#currentWeather");
+var cityGiven = document.querySelector("#cityShown");
+var fiveDayEl = document.querySelector("#forecast");
+var fiveDayFore = document.querySelector("#fiveDayForecast");
+var pastWeathersEl = document.querySelector("#pastWeathers");
